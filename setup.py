2ea3b50036841c46b92b74136bc5d03e0bb038da
dev1145 <35825097+dev1145@users.noreply.github.com>
2022-11-03 17:06:33 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# please install python if it is not present in the system
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+ name='timeloop',
+ version='1.1',
+ packages=['hello-world'],
+ license = 'MIT',
+ description = 'hello world.',
+ author = 'dev1145',
+ author_email = '@gmail.com',
+ keywords = ['hello-world','hello world'],
+ long_description=long_description,
+ long_description_content_type="text/markdown",
+ url="https://github.com/dev1145/hello-world",
+ install_requires=[ ],
+ include_package_data=True,
+)
